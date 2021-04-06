0ee0a6b356791ec5636b1d5a5df46ea018307484
IKHYEON KIM <behonz@gmail.com>
2021-04-06 09:08:53 +0900

    commit from BASE

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import PropsComponent from './03/PropsComponent';
+import ChildComponent from './03/ChildComponent';
+import StateExample from './03/StateExample';
+import ForceUpdateExample from './03/ForceUpdateExample';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="title">
+        <PropsComponent name="hello" />
+        <ChildComponent 
+          requiredStringValue="required value"
+          objValue={{name:'test', age:12}} />
+        <StateExample />
+        <ForceUpdateExample />
       </div>
     );
   }
