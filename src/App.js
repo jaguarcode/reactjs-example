f0307925310e13707a1372be24a79fbf921a06a5
IKHYEON KIM <behonz@gmail.com>
2021-04-06 19:58:22 +0900

    Material Design 적용 (SCSS 소스)

--- src/App.js
+++ src/App.js
@@ -1,20 +1,17 @@
 import React, { Component } from 'react';
+import './sass/materialize.scss';
 import './App.css';
-import PropsComponent from './03/PropsComponent';
-import ChildComponent from './03/ChildComponent';
-import StateExample from './03/StateExample';
-import ForceUpdateExample from './03/ForceUpdateExample';
 
 class App extends Component {
   render() {
     return (
-      <div className="title">
-        <PropsComponent name="hello" />
-        <ChildComponent 
-          requiredStringValue="required value"
-          objValue={{name:'test', age:12}} />
-        <StateExample />
-        <ForceUpdateExample />
+      <div>
+        <nav>
+          <div className="nav-wrapper">
+            <div>Hello, Material Design!</div>
+          </div>
+        </nav>
+        <h1>Material</h1>
       </div>
     );
   }
