a5bbb83d7ce8e7781a837564ef3ce1696ed88778
IKHYEON KIM <behonz@gmail.com>
2021-04-10 01:02:32 +0900

    JEST 및 enzyme 라이브러리 적용

---
+++ src/__test__/04/Input.test.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import Input from '../../03/Input';
+
+describe('<Input>', () => {
+  it('renders without crashing', () => {
+    expect(() => {
+      shallow(<Input name="test_name" />);
+    }).not.toThrow();
+  });
+});
+
+describe('<Input>', () => {
+  it('has one element', () => {
+    const wrapper = shallow(<Input name="test_name" />);
+    expect(wrapper.length).toEqual(1);
+    expect(wrapper).toHaveLength(1);
+  });
+});
