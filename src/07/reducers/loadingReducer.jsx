d3a3871027e67fabd4b423cb96f216e6f76a0b7a
IKHYEON KIM <behonz@gmail.com>
2021-04-14 18:40:05 +0900

    리듀서 반환값 재구성

--- src/07/reducers/loadingReducer.jsx
+++ src/07/reducers/loadingReducer.jsx
@@ -1,11 +1,13 @@
-export default function reducer(state = {}, action) {
+const initState = false;
+
+export default function reducer(state = initState, action) {
   const { type, payload } = action;
   switch (type) {
     case 'SET_LOADING': {
-      return { ...state, loading: payload };
+      return payload;
     }
     case 'RESET_LOADING': {
-      return { ...state, loading: false };
+      return initState;
     }
     default:
       return state;
